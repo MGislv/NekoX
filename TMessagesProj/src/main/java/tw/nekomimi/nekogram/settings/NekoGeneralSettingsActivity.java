b1318961808e361f92a1b08b260843df6ae1abb5
MGislv <mgislv@ceppo.xyz>
2022-02-06 23:34:43 +0100

    Re-enable censored features

--- TMessagesProj/src/main/java/tw/nekomimi/nekogram/settings/NekoGeneralSettingsActivity.java
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/settings/NekoGeneralSettingsActivity.java
@@ -721,9 +721,6 @@ public class NekoGeneralSettingsActivity extends BaseFragment {
     }
 
     private void setCanNotChange() {
-        if (!NekoXConfig.isDeveloper())
-            cellGroup.rows.remove(hideSponsoredMessageRow);
-
         boolean enabled;
 
         enabled = NekoConfig.largeAvatarInDrawer.Int() > 0;
